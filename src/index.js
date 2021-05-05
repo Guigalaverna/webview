fda109abd6ea0042db7e9994a4890f0fbadbd7d5
Guilherme Galaverna <guigalaverna@gmail.com>
2021-05-05 13:51:11 -0300

    Finished app

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const settings = require('../settings.json')
+
+let mainWindow
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    ...settings.window,
+  })
+
+  mainWindow.loadURL(settings.url)
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
